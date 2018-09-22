c65c97b08795b3bcf74fd96ee3f13fb65d3f5fd1
willyspinner <wilsonjusuf1998@gmail.com>
2018-09-22 23:18:46 +0700

    location path typo fix

--- src/commands/script_commands.js
+++ src/commands/script_commands.js
@@ -6,7 +6,7 @@ put exec_scripts in exec_scripts directory, located in root..
 const {execSimpleCommandArrArgs}  = require('../shellutils');
 module.exports = async (arr_message) => {
     if (arr_message[0] === "exec" && arr_message.length >= 2){
-        const {stderr,stdout} = await execSimpleCommandArrArgs(`${__dirname}/../exec_scripts/${arr_message[1]}`, arr_message.slice(2));
+        const {stderr,stdout} = await execSimpleCommandArrArgs(`${__dirname}/../../exec_scripts/${arr_message[1]}`, arr_message.slice(2));
         if (stderr !== ''){
             return stdout+stderr;
         }
